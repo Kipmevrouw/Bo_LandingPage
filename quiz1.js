67e9f55a2aa75ce4e779b44550b3dcccfd752e62
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-01-14 10:01:39 +0100

    Laatste bewerkingen

---
+++ quiz1.js
@@ -0,0 +1,73 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+		"title": "Welk kunstwerk ben jij?",
+		"imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Houd u van hout?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": true
+					},
+					{
+						"answer": "Nee",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Houd u van fruit?",
+				"answers": [{
+						"answer": "Ja",
+						"feedback": false
+					},
+					{
+						"answer": "Nee",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Bent u liever groot of klein?",
+				"answers": [{
+						"answer": "Groot",
+						"feedback": true
+					},
+					{
+						"answer": "Klein",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Staat u liever buiten of binnen?",
+				"answers": [{
+						"answer": "Buiten",
+						"feedback": false
+					},
+					{
+						"answer": "Binnen",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Bent u liever een object of persoon?",
+				"answers": [{
+						"answer": "Object",
+						"feedback": true
+					},
+					{
+						"answer": "Persoon",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
